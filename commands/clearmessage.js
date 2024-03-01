3087fdd4566415ee17d03b09c7635aa7c9c429ac
Tutorial RJ <75653910+Tutorial-RJ@users.noreply.github.com>
2024-03-01 12:52:43 +0530

    Update clearmessage.js

--- commands/clearmessage.js
+++ commands/clearmessage.js
@@ -1,49 +1,44 @@
-const { Client, Intents, Collection } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const fs = require('fs');
+const { EmbedBuilder } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+module.exports = {
+  data: {
+    name: 'clearmessage',
+    description: 'Clears the specified number of messages in the channel.',
+    options: [
+      {
+        name: 'amount',
+        type: 'INTEGER',
+        description: 'The number of messages to clear (up to 100).',
+        required: true,
+      },
+    ],
+  },
+  async execute(interaction) {
+    if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+      return interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+    }
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    const amount = interaction.options.getInteger('amount');
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
-}
+    if (amount <= 0 || amount > 100) {
+      return interaction.reply({ content: 'You must provide a number between 1 and 100.', ephemeral: true });
+    }
 
-const commands = client.commands.map(command => command.data.toJSON());
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+    try {
+      await interaction.channel.bulkDelete(amount);
+      const embed = new EmbedBuilder()
+        .setColor('#3498db')
+        .setAuthor({
+          name: 'Cleared Messages',
+          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157314241393598585/4618-no-slides.png?ex=65182861&is=6516d6e1&hm=dac8fed5a18e1574485e833d4c017591c50f59d161e1bde7fed5f6a92543f951&',
+          url: 'https://discord.gg/FUEHs7RCqz'
+        })
+        .setDescription(`**${amount} messages cleared successfully.**`);
 
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(
-      Routes.applicationGuildCommands('YOUR_CLIENT_ID', 'YOUR_GUILD_ID'),
-      { body: commands },
-    );
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (!client.commands.has(commandName)) return;
-
-  try {
-    await client.commands.get(commandName).execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-  }
-});
-
-client.login(process.env.TOKEN);
+      interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error('Error while clearing messages:', error);
+      interaction.reply({ content: 'An error occurred while clearing messages.', ephemeral: true });
+    }
+  },
+};
