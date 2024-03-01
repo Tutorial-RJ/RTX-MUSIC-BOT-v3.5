a2bc76c1a56af80e0551c91e638b6f0f937cd774
Tutorial RJ <75653910+Tutorial-RJ@users.noreply.github.com>
2024-03-01 12:20:17 +0530

    Update clearmessage.js

--- commands/clearmessage.js
+++ commands/clearmessage.js
@@ -1,1 +1,30 @@
 
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('!clear')) {
+    const args = message.content.split(' ');
+    const amount = parseInt(args[1]);
+
+    if (isNaN(amount) || amount <= 0) {
+      return message.reply('Please provide a valid number of messages to clear.');
+    }
+
+    if (amount > 100) {
+      return message.reply('You can only delete up to 100 messages at a time.');
+    }
+
+    await message.channel.bulkDelete(amount + 1);
+    const embed = new MessageEmbed()
+      .setColor('#ff0000')
+      .setDescription(`Cleared ${amount} messages.`);
+    message.channel.send({ embeds: [embed] });
+  }
+});
+
+client.login('YOUR_BOT_TOKEN');
